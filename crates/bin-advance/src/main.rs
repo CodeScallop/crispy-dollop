d8165f4d4f0d14e896a210c3b04bca933623d05f
Alex Tran <hoangpichgoodkid@gmail.com>
2024-01-01 23:14:20 +0700

    Updated examples code

--- crates/bin-advance/src/main.rs
+++ crates/bin-advance/src/main.rs
@@ -365,64 +365,76 @@
 //     println!("{}",emp.name);
 // }
 
-enum Colors {
-    Red,
-    Blue,
-    White,
+// enum Colors {
+//     Red,
+//     Blue,
+//     White,
+// }
+
+// impl Colors {
+//     fn print(&self){
+//         match self {
+//             Colors::Red => println!("red"),
+//             Colors::Blue => println!("Blue"),
+//             Colors::White => println!("White"),
+
+//         }
+//     }
+// }
+
+// struct Persons {
+//     age: i32,
+//     name: String,
+//     fa_color: Colors,
+// }
+
+// impl Persons {
+//     fn print(&self){
+//         println!("{:?},{:?}",self.age,self.name);
+//         self.fa_color.print();
+//     }
+// }
+
+
+// fn main(){
+
+//     let person_init = vec![
+//         Persons {
+//             name: "Pich".to_owned(),
+//             age: 9,
+//             fa_color: Colors::Red,
+//         },
+
+//         Persons {
+//             name: "Hoang".to_owned(),
+//             age: 12,
+//             fa_color: Colors::Blue,
+//         },
+        
+//         Persons {
+//             name: "Tran".to_owned(),
+//             age: 4,
+//             fa_color: Colors::White,
+//         },
+//     ];
+
+//     for person in person_init {
+//         if person.age <= 10 {
+//             person.print();
+//         }
+//     }
+
+
+// }
+
+
+//enum advange
+enum Ticket {
+    Backstage(f64, String),
+    Vip(f64, String),
+    Standard(f64),
 }
-
-impl Colors {
-    fn print(&self){
-        match self {
-            Colors::Red => println!("red"),
-            Colors::Blue => println!("Blue"),
-            Colors::White => println!("White"),
-
-        }
-    }
-}
-
-struct Persons {
-    age: i32,
-    name: String,
-    fa_color: Colors,
-}
-
-impl Persons {
-    fn print(&self){
-        println!("{:?},{:?}",self.age,self.name);
-        self.fa_color.print();
-    }
-}
-
 
 fn main(){
-
-    let person_init = vec![
-        Persons {
-            name: "Pich".to_owned(),
-            age: 9,
-            fa_color: Colors::Red,
-        },
-
-        Persons {
-            name: "Hoang".to_owned(),
-            age: 12,
-            fa_color: Colors::Blue,
-        },
-        
-        Persons {
-            name: "Tran".to_owned(),
-            age: 4,
-            fa_color: Colors::White,
-        },
-    ];
-
-    for person in person_init {
-        if person.age <= 10 {
-            person.print();
-        }
-    }
-
 
 }
