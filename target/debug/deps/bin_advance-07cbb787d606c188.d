1a8c613c93ecdbcd2987cf47ae1acf6000b276c5
Alex Tran <hoangpichgoodkid@gmail.com>
2024-01-02 20:10:40 +0700

    Update

---
+++ target/debug/deps/bin_advance-07cbb787d606c188.d
@@ -0,0 +1,5 @@
+D:\Web3\RUST\rust-learning-api\target\debug\deps\libbin_advance-07cbb787d606c188.rmeta: crates\bin-advance\src\main.rs
+
+D:\Web3\RUST\rust-learning-api\target\debug\deps\bin_advance-07cbb787d606c188.d: crates\bin-advance\src\main.rs
+
+crates\bin-advance\src\main.rs:
---
+++ target/debug/deps/ppv_lite86-099f57a3775acbc4.d
@@ -0,0 +1,9 @@
+D:\Web3\RUST\rust-learning-api\target\debug\deps\libppv_lite86-099f57a3775acbc4.rmeta: C:\Users\hoang\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\lib.rs C:\Users\hoang\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\soft.rs C:\Users\hoang\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\types.rs C:\Users\hoang\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\x86_64\mod.rs C:\Users\hoang\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\x86_64\sse2.rs
+
+D:\Web3\RUST\rust-learning-api\target\debug\deps\ppv_lite86-099f57a3775acbc4.d: C:\Users\hoang\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\lib.rs C:\Users\hoang\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\soft.rs C:\Users\hoang\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\types.rs C:\Users\hoang\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\x86_64\mod.rs C:\Users\hoang\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\x86_64\sse2.rs
+
+C:\Users\hoang\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\lib.rs:
+C:\Users\hoang\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\soft.rs:
+C:\Users\hoang\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\types.rs:
+C:\Users\hoang\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\x86_64\mod.rs:
+C:\Users\hoang\.cargo\registry\src\index.crates.io-6f17d22bba15001f\ppv-lite86-0.2.17\src\x86_64\sse2.rs:
